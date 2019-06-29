5e8d9c91f0a5e90558c73b6f33811a4487712003
Badmon <jleonh@autonoma.edu.pe>
2019-06-29 01:36:55 -0500

    Commit

--- app/src/main/java/com/example/glide/main.java
+++ app/src/main/java/com/example/glide/main.java
@@ -9,19 +9,20 @@ import com.bumptech.glide.Glide;
 
 public class main extends AppCompatActivity {
 
-    private ImageView img;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        img = findViewById(R.id.img);
+        image= findViewById(R.id.img);
 
-        String url=" ";
+        String url="http://hitexroidgroup.ir/uploads/monthly_2017_12/building-image-gallery-all-with-glide.jpg.dcbab30c0c25224e1ae0b50104b458d7.jpg";
+        String url1="https://cdn.vox-cdn.com/thumbor/dRlxNheMNdTmTFZg7bHh-ZnoNis=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/14622892/Switch_PokemonLetsGo_090618_PressKit_SCRN_10.jpg";
 
         Glide.with(this).
-                load(url).
-                into(img);
+                load(url1).
+                into(image);
     }
 }
